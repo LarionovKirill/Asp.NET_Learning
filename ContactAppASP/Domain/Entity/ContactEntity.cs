5fda1d001bf02b90247e08ff9583430c84f90388
LarionovKirill <lari-kir2013@mail.ru>
2023-11-14 15:08:22 +0700

    feature(ContactAppASP): Добавлены классы для работы с базой данных

---
+++ ContactAppASP/Domain/Entity/ContactEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace ContactDomain.Entity
+{
+    public class ContactEntity
+    {
+        /// <summary>
+        /// Свойство Id контакта.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Свойство имени в контакте.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Свойство Email в контакте.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Свойство номера телефона в контакте.
+        /// </summary>
+        public string Phone { get; set; }
+    }
+}
---
+++ ContactAppASP/appDbContext/appDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactDomain.Entity;
+
+namespace Contact.DAL
+{
+    public class appDbContext : DbContext
+    {
+
+        public appDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ContactEntity> Contacts { get; set; }
+    }
+}
