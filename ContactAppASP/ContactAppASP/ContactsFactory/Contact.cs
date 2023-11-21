8c5a13dd0e9bc196f5a6261f9bef9d2ea1c9c1ed
LarionovKirill <lari-kir2013@mail.ru>
2023-11-21 22:37:53 +0700

    feature(Contact.cs): временно создал класс контакт для работы с сервером без бд.

---
+++ ContactAppASP/ContactAppASP/ContactsFactory/Contact.cs
@@ -0,0 +1,41 @@
+﻿namespace ContactAppASP.ContactsFactory
+{
+    public class Contact
+    {
+        /// <summary>
+        /// Свойство имени в контакте.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Свойство Email в контакте.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Свойство номера телефона в контакте.
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Конструктор без параметров.
+        /// </summary>
+        public Contact()
+        {
+            
+        }
+
+        /// <summary>
+        /// Конструктор класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="name">Имя контакта.</param>
+        /// <param name="email">Email контакта.</param>
+        /// <param name="phone">Номер контакта.</param>
+        public Contact(string name, string email, string phone)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
