e64ca11ca7335dec52ca21bf5768399c2038ecc9
LarionovKirill <lari-kir2013@mail.ru>
2023-11-25 12:00:51 +0700

    refactoring(_Layout.cs): скрипты вынесены в отдельные файлы.

---
+++ ContactAppASP/ContactAppASP/wwwroot/js/EditContact.js
@@ -0,0 +1,20 @@
+﻿document.querySelector('[type="file"]').addEventListener('change', (e) => {
+    const elFileImg = document.querySelector('.avatar');
+    const files = e.target.files;
+    const countFiles = files.length;
+    if (!countFiles) {
+        alert('Не выбран файл!');
+        return;
+    }
+    const selectedFile = files[0];
+    if (!/^image/.test(selectedFile.type)) {
+        alert('Выбранный файл не является изображением!');
+        return;
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(selectedFile);
+    reader.addEventListener('load', (e) => {
+        elFileImg.src = e.target.result;
+        elFileImg.alt = selectedFile.name;
+    });
+});
---
+++ ContactAppASP/ContactAppASP/wwwroot/js/Index.js
@@ -0,0 +1,58 @@
+﻿function ShowInfo() {
+    var select = document.querySelector('select');
+    var index = select.selectedIndex;
+    if (index < 0) {
+        alert('Выберите пользователя');
+        return;
+    }
+    $.ajax({
+        method: "GET",
+        url: "Home/GetContact?index=" + index,
+        success: function (data) {
+            $(".container").html(data);
+            select = document.querySelector('select');
+            select.selectedIndex = index;
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    })
+}
+
+function EditContact() {
+    var select = document.querySelector('select');
+    var index = select.selectedIndex;
+    if (index < 0) {
+        alert('Выберите пользователя');
+        return;
+    }
+    $.ajax({
+        method: "GET",
+        url: "Home/EditContact?index=" + index,
+        success: function (data) {
+            $(".container").html(data);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    })
+}
+
+function RemoveContact() {
+    var select = document.querySelector('select');
+    var index = select.selectedIndex;
+    if (index < 0) {
+        alert('Выберите пользователя');
+        return;
+    }
+    $.ajax({
+        method: "GET",
+        url: "Home/RemoveContact?index=" + index,
+        success: function (data) {
+            $(".container").html(data);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    })
+}
