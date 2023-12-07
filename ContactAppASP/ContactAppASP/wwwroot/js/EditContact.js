704610c9b11fb86f4bfa7dbe2c6df6a69095d920
LarionovKirill <lari-kir2013@mail.ru>
2023-12-07 19:27:09 +0700

    fix(EditContact.js): испарвил код изменения фото.

--- ContactAppASP/ContactAppASP/wwwroot/js/EditContact.js
+++ ContactAppASP/ContactAppASP/wwwroot/js/EditContact.js
@@ -13,13 +13,12 @@ const inputFile = document.getElementById("input-file");
  */
 const imageView = document.getElementById("img-view");
 
-
 /**
  * Загружает фото по url.
  */
 function uploadImage() {
     let imgLink = URL.createObjectURL(inputFile.files[0]);
-    imageView.style.backgroundImage = `url(${imgLink})`;
+    imageView.src = imgLink;
     imageView.textContent = "";
     imageView.style.border = 0;
 }
