34347accbe728b761bce034ee208f125a5879648
LarionovKirill <lari-kir2013@mail.ru>
2023-12-17 20:05:15 +0700

    feature(Contactvalidation.js): добавлена валидация полей

---
+++ ContactAppASP/ContactAppASP/wwwroot/js/ContactValidation.js
@@ -0,0 +1,33 @@
+﻿const saveButton = document.getElementById("saveBut");
+const nameInput = document.getElementById("name");
+const phoneInput = document.getElementById("phone");
+const emailInput = document.getElementById("email");
+
+var mask = new IMask(phoneInput, {
+    mask: '+{7} (000) 000-00-00',
+    lazy: false,  // make placeholder always visible
+});
+
+function nameValidation()
+{
+    if (nameInput.value.length > 100)
+    {
+        nameInput.style.backgroundColor = "#F08080";
+        return;
+    }
+    nameInput.style.backgroundColor = "white";
+}
+
+nameInput.addEventListener("input", nameValidation);
+
+function emailValidation() {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (emailInput.value.length > 100 || reg.test(emailInput.value) == false)
+    {
+        emailInput.style.backgroundColor = "#F08080";
+        return;
+    }
+    emailInput.style.backgroundColor = "white";
+}
+
+emailInput.addEventListener("input", emailValidation);
